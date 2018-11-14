ebc0693c771b439f44b004efb7928216e99c3bb3
923226145 <923226145@qq.com>
2018-11-14 23:20:09 +0800

    短信验证码登录

--- chapter5/src/main/java/com/lzc/security/validate/code/sms/SmsCodeProcessor.java
+++ chapter5/src/main/java/com/lzc/security/validate/code/sms/SmsCodeProcessor.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.context.request.ServletWebRequest;
-
+// 短信验证码生成器
 @Component("smsCodeProcessor")
 public class SmsCodeProcessor extends AbstractValidateCodeProcessor<SmsCode> {
 
