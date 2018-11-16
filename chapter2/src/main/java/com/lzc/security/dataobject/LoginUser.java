c1391331fa687131667ac49e348f6b93f9df39a6
923226145 <923226145@qq.com>
2018-11-16 10:43:53 +0800

    基于数据库用户登录

--- chapter2/src/main/java/com/lzc/security/dataobject/LoginUser.java
+++ chapter2/src/main/java/com/lzc/security/dataobject/LoginUser.java
@@ -1,78 +1,24 @@
 package com.lzc.security.dataobject;
 
-import org.springframework.security.core.CredentialsContainer;
+
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
+import org.springframework.security.core.userdetails.User;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
-public class LoginUser implements UserDetails, CredentialsContainer {
-
-    private String password;
-    private  String username;
-    private  Set<GrantedAuthority> authorities;
-    private  boolean accountNonExpired;
-    private  boolean accountNonLocked;
-    private  boolean credentialsNonExpired;
-    private  boolean enabled;
+public class LoginUser extends User {
 
     private String nickName;
 
     public LoginUser() {
-        this("", "", true, true, true, true, null);
+        super("", "", true, true, true, true, null);
     }
 
     public LoginUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
-        this(username, password, true, true, true, true, authorities);
+        super(username, password, true, true, true, true, authorities);
     }
 
     public LoginUser(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
-        if (username != null && !"".equals(username) && password != null) {
-            this.username = username;
-            this.password = password;
-            this.enabled = enabled;
-            this.accountNonExpired = accountNonExpired;
-            this.credentialsNonExpired = credentialsNonExpired;
-            this.accountNonLocked = accountNonLocked;
-
-            this.authorities = new HashSet<>();
-            for (GrantedAuthority g : authorities) {
-                this.authorities.add(g);
-            }
-
-        } else {
-            throw new IllegalArgumentException("Cannot pass null or empty values to constructor");
-        }
-    }
-
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public Set<GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    public void setAuthorities(Set<GrantedAuthority> authorities) {
-        this.authorities = authorities;
+        super(username,password,enabled,accountNonExpired,credentialsNonExpired,accountNonLocked,authorities);
     }
 
     public String getNickName() {
@@ -81,30 +27,5 @@ public class LoginUser implements UserDetails, CredentialsContainer {
 
     public void setNickName(String nickName) {
         this.nickName = nickName;
-    }
-
-    @Override
-    public void eraseCredentials() {
-        this.password = null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return this.accountNonExpired;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return this.accountNonLocked;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return this.credentialsNonExpired;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return this.enabled;
     }
 }
